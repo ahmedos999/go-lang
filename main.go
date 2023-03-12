6dea92db5815548750e1cca4d94d03264e8fadd6
ahmed osman <ahmedos099912@gmail.com>
2023-03-12 10:49:45 +0200

    arrays

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var nameOne = "one"
-	var nameTwo = "Two"
-	nameOne = "three"
-	nameThree := "four"
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// var nameOne = "one"
+	// var nameTwo = "Two"
+	// nameOne = "three"
+	// nameThree := "four"
+	var name string = "ahmed"
+	var age int = 24
+	var forrmated = fmt.Sprintf("My name is %v and my age is %v", name, age)
+	fmt.Println(forrmated)
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	var ages = [3]int{1, 2, 3}
+
+	fmt.Println(ages, len(ages))
 }
