ee3d6f9cd8494a56bf114e68d91405533d4efd81
ahmed osma (R) <ahmedos099912@gmail.com>
2023-03-13 14:20:33 +0200

    struct type

--- main.go
+++ main.go
@@ -1,32 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
-func cycleNames(n []string, f func(string)) {
-	for _, v := range n {
-		f(v)
-	}
-}
-
-func sayGreeting(n string) {
-	fmt.Printf("good morning %v \n", n)
-}
+import "fmt"
 
 func main() {
-
-	menu := map[string]float64{
-		"soup":           4.99,
-		"pie":            7.99,
-		"salad":          6.99,
-		"toffee pudding": 3.55,
-	}
-
-	fmt.Println(menu)
-	fmt.Println(menu["pie"])
-
-	for k, v := range menu {
-		fmt.Println(k, ":", v)
-	}
+	mybill := newBill("mario's bill")
+	fmt.Println(mybill)
 }
