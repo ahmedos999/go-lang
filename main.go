ed15d0e6beb11f86307b9f64648be1ed8d8e6883
ahmed osman <ahmedos099912@gmail.com>
2023-03-14 10:21:26 +0200

    solve marge conflict

--- main.go
+++ main.go
@@ -3,13 +3,8 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-	names := []string{"ahmed", "ali", "khalid"}
-	cycleNames(names, sayGreeting)
-=======
 	mybill := newBill("mario's bill")
 	fmt.Println(mybill)
 
 	fmt.Println(mybill.format())
->>>>>>> 3c91b09161518188470eab7c51d8cb74791769ee
 }
